be75860a194b2c88f9560c98fc2173084acbc4a1
Denny <yesmehfriend@googlemail.com>
2011-10-23 20:34:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dwitter::Application.config.secret_token = '13f77828ccf84421bce9e172a5ad08b329222a7c0d34bf761e003e2cf2092cb808b97e92528b2768802826bf7907605c9c19057701087ed5d79ddb813e2159c4'
